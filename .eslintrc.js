f66cd2721c228d8af41bbe30140e7b69d940a1ba
bglim <bglim@ui-lab.co.kr>
2021-03-07 11:33:06 +0900

    setup starbucks_vue

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/vue3-strongly-recommended",
+    // "plugin:vue/vue3-recommended"
+    // 'plugin:vue/vue3-essential',
+    // '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "brace-style": [
+      'error',
+      '1tbs'
+    ],
+    "indent": ["error", 2],
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          'el',
+          'name',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'fetch',
+          'asyncData',
+          'data',
+          'computed',
+          'watch',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          'head',
+          ['template', 'render'],
+          'renderError',
+        ],
+      },
+    ],
+    
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'UNIQUE',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+      },
+    ],
+  }
+}
