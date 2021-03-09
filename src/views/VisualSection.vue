e02944f19900b6f0a6a579d10c8b815c2b168110
bglim <bglim@ui-lab.co.kr>
2021-03-09 12:22:13 +0900

    비주얼섹션 페이드 기능 추가 및 테스트 ( 컴포넌트화 전 )

--- src/views/VisualSection.vue
+++ src/views/VisualSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="visual">
     <div class="inner">
-      <div class="title">
+      <div class="title fade-in">
         <img
           src="@/assets/images/visual_title.png"
           alt="STARBUCKS DELIGHTFUL START TO THE YEARS"
@@ -13,38 +13,57 @@
           자세히 보기
         </a> 
       </div>
-      <img
-        src="@/assets/images/visual_cup1.png"
-        alt="NEW OATMEAL LATTE"
-        class="cup1 image"
-      >
-      <img
-        src="@/assets/images/visual_cup1_text.png"
-        alt="오트밀 라떼"
-        class="cup1 text"
-      >
-      <img
-        src="@/assets/images/visual_cup2.png"
-        alt="NEW STARBUCKS CARAMEL CRUMBLE MOCHA"
-        class="cup2 image"
-      >
-      <img
-        src="@/assets/images/visual_cup2_text.png"
-        alt="스타벅스 카라멜 크럼블 모카"
-        class="cup2 text"
-      >
-      <img
-        src="@/assets/images/visual_spoon.png"
-        alt="Spoon"
-        class="spoon"
-      >
+      <div class="fade-in">
+        <img
+          src="@/assets/images/visual_cup1.png"
+          alt="NEW OATMEAL LATTE"
+          class="cup1 image"
+        >
+        <img
+          src="@/assets/images/visual_cup1_text.png"
+          alt="오트밀 라떼"
+          class="cup1 text"
+        >
+      </div>
+      <div class="fade-in">
+        <img
+          src="@/assets/images/visual_cup2.png"
+          alt="NEW STARBUCKS CARAMEL CRUMBLE MOCHA"
+          class="cup2 image"
+        >
+        <img
+          src="@/assets/images/visual_cup2_text.png"
+          alt="스타벅스 카라멜 크럼블 모카"
+          class="cup2 text"
+        >
+      </div>
+      <div class="fade-in">
+        <img
+          src="@/assets/images/visual_spoon.png"
+          alt="Spoon"
+          class="spoon"
+        >
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import gsap from "gsap";
+
 export default {
   name: "VisualSection",
+  mounted() {
+    const fadeEls = document.querySelectorAll('.visual .fade-in');
+    fadeEls.forEach(function (fadeEl, index) {
+      gsap.to(fadeEl, 1, 
+        {
+          delay: (index + 1) *  .7,
+          opacity : 1
+        }
+      );
+    })
+  }
 }
 </script>
 
