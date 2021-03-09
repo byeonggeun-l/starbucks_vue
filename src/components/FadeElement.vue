2e3a4a4280a156f919a22f19b4287731a25aa2dc
bglim <bglim@ui-lab.co.kr>
2021-03-09 15:28:32 +0900

    FadeElement 컴포넌트화 완료 및 테스트.

---
+++ src/components/FadeElement.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    ref="refFadeInEl"
+    class="fade-in"
+  >
+    <img
+      v-if="elTagName === 'img'"
+      :src="require(`@/assets/${imgTagSrc}`)"
+      :alt="imgTagAlt"
+      :class="classList"
+    >
+    <a 
+      v-else-if="elTagName === 'a'"
+      href="javascript:void(0)"
+      :class="classList"
+    >
+      {{ aTagInnerText }}
+    </a>
+  </div>
+</template>
+
+<script>
+import gsap from "gsap";
+
+export default {
+  name: 'Fadeelement',
+  props: {
+    elTagName: {
+      type: String,
+      required: true
+    },
+    fadeSec: {
+      type: Number,
+      required: true
+    },
+    classList: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    imgTagSrc:{
+      type: String,
+      required: false,
+      default: "",
+    },
+    imgTagAlt:{
+      type: String,
+      required: false,
+      default: "",
+    },
+    aTagInnerText:{
+      type: String,
+      required: false,
+      default: "",
+    }
+  },
+  mounted() {
+    const fadeEl = this.$refs.refFadeInEl;
+    gsap.to(fadeEl, 1, 
+      {
+        delay: this.fadeSec * .8,
+        opacity : 1
+      }
+    );
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/VisualSection.vue
+++ src/views/VisualSection.vue
@@ -1,68 +1,84 @@
 <template>
   <section class="visual">
     <div class="inner">
-      <div class="title fade-in">
-        <img
-          src="@/assets/images/visual_title.png"
-          alt="STARBUCKS DELIGHTFUL START TO THE YEARS"
-        >
-        <a
-          href="javascript:void(0)"
-          class="btn btn--brown"
-        >
-          자세히 보기
-        </a> 
+      <div class="title">
+        <FadeElement 
+          :el-tag-name="'img'"
+          :img-tag-src="'images/visual_title.png'"
+          :img-tag-alt="'STARBUCKS DELIGHTFUL START TO THE YEARS'"
+          :fade-sec="1"
+        />
+        <FadeElement 
+          :el-tag-name="'a'"
+          :class-list="'btn btn--brown'"
+          :a-tag-inner-text="'자세히 보기'"
+          :fade-sec="1"
+        />
       </div>
-      <div class="fade-in">
-        <img
-          src="@/assets/images/visual_cup1.png"
-          alt="NEW OATMEAL LATTE"
-          class="cup1 image"
-        >
-        <img
-          src="@/assets/images/visual_cup1_text.png"
-          alt="오트밀 라떼"
-          class="cup1 text"
-        >
+      <div>
+        <FadeElement 
+          :el-tag-name="'img'"
+          :class-list="'cup1 image'"
+          :img-tag-src="'images/visual_cup1.png'"
+          :img-tag-alt="'NEW OATMEAL LATTE'"
+          :fade-sec="2"
+        />
+        <FadeElement 
+          :el-tag-name="'img'"
+          :class-list="'cup1 text'"
+          :img-tag-src="'images/visual_cup1_text.png'"
+          :img-tag-alt="'오트밀 라떼'"
+          :fade-sec="2"
+        />
       </div>
-      <div class="fade-in">
-        <img
-          src="@/assets/images/visual_cup2.png"
-          alt="NEW STARBUCKS CARAMEL CRUMBLE MOCHA"
-          class="cup2 image"
-        >
-        <img
-          src="@/assets/images/visual_cup2_text.png"
-          alt="스타벅스 카라멜 크럼블 모카"
-          class="cup2 text"
-        >
+      <div>
+        <FadeElement 
+          :el-tag-name="'img'"
+          :class-list="'cup2 image'"
+          :img-tag-src="'images/visual_cup2.png'"
+          :img-tag-alt="'NEW STARBUCKS CARAMEL CRUMBLE MOCHA'"
+          :fade-sec="3"
+        />
+        <FadeElement 
+          :el-tag-name="'img'"
+          :class-list="'cup2 text'"
+          :img-tag-src="'images/visual_cup2_text.png'"
+          :img-tag-alt="'스타벅스 카라멜 크럼블 모카'"
+          :fade-sec="3"
+        />
       </div>
-      <div class="fade-in">
-        <img
-          src="@/assets/images/visual_spoon.png"
-          alt="Spoon"
-          class="spoon"
-        >
+      <div>
+        <FadeElement
+          :el-tag-name="'img'"
+          :class-list="'spoon'"
+          :img-tag-src="'images/visual_spoon.png'"
+          :img-tag-alt="'Spoon'"
+          :fade-sec="4"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import FadeElement from '@/components/FadeElement.vue'
 import gsap from "gsap";
 
 export default {
   name: "VisualSection",
+  components:{
+    FadeElement,
+  },
   mounted() {
-    const fadeEls = document.querySelectorAll('.visual .fade-in');
-    fadeEls.forEach(function (fadeEl, index) {
-      gsap.to(fadeEl, 1, 
-        {
-          delay: (index + 1) *  .7,
-          opacity : 1
-        }
-      );
-    })
+    // const fadeEls = document.querySelectorAll('.visual .fade-in');
+    // fadeEls.forEach(function (fadeEl, index) {
+    //   gsap.to(fadeEl, 1, 
+    //     {
+    //       delay: (index + 1) *  .7,
+    //       opacity : 1
+    //     }
+    //   );
+    // })
   }
 }
 </script>
